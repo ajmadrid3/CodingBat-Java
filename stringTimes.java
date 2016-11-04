d061b6c4c7160eb61c06d54ccd73f399e5cae568
Andrew Madrid <ajmadrid3@miners.utep.edu>
2016-11-04 15:18:56 -0600

    Added stringTimes.java

---
+++ stringTimes.java
@@ -0,0 +1,28 @@
+public class stringTimes {
+    public String stringTimes(String str, int n) {
+        if(n == 0)
+        {
+            String product = "";
+            return product;
+        }
+        else if(n == 1)
+        {
+            return str;
+        }
+        else
+        {
+            String product = str;
+            for(int i = 1; i < n; i++)
+            {
+                product = product + str;
+            }
+            return product;
+        } 
+    }
+
+    public static void main(String[] args) {
+        System.out.println(stringTimes("Hi", 2));
+        System.out.println(stringTimes("Hi, 0"));
+        System.out.println(stringTimes("code", 4));
+    }
+}
